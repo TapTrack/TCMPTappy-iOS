3e9b0957a4a0fa971d05bd47eba72a32a44b205b
David Shalaby <dshalaby@davids-mac-mini.local>
2018-03-06 20:18:12 -0500

    Initial Commit

---
+++ TCMP/TCMP.h
@@ -0,0 +1,13 @@
+//
+//  TCMP.h
+//  TCMP
+//
+//  Created by David Shalaby on 2018-03-06.
+//  Copyright © 2018 David Shalaby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCMP : NSObject
+
+@end
---
+++ TCMP/TCMP.m
@@ -0,0 +1,13 @@
+//
+//  TCMP.m
+//  TCMP
+//
+//  Created by David Shalaby on 2018-03-06.
+//  Copyright © 2018 David Shalaby. All rights reserved.
+//
+
+#import "TCMP.h"
+
+@implementation TCMP
+
+@end
