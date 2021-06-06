b9353d165f18d4bf581d24e7ca293103e9e99498
dshalaby <dave@taptrack.com>
2021-06-06 19:15:34 -0400

    add Cocoapods

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'TapTrackReader' do
+  use_frameworks!
+  pod 'SwiftTLV', :path=> '/Users/Shared/Xcode Projects/SwiftTLV'
+end
---
+++ Pods/Target Support Files/Pods-TapTrackReader/Pods-TapTrackReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TapTrackReader : NSObject
+@end
+@implementation PodsDummy_Pods_TapTrackReader
+@end
